7c0b3a1489cf2ba2d597c3a2c2b56e4d84bc8562
Maggie Sun <93930210+maggiesun1618@users.noreply.github.com>
2021-11-25 05:25:48 +0000

    I had the program print 31 if the inputted month was January, March, May, July, August, October, or December; 30 if the inputted month was April, June, September, or November; 28 or 29 if the inputted month was February; and "not a month" otherwise.

--- part4.py
+++ part4.py
@@ -28,10 +28,10 @@ num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
 
 if num1 > 0 and num2 > 0:
-  print(''"positive"')
+  print('"positive"')
 elif num1 < 0 and num2 < 0:
-  print(''"negative"')
+  print('"negative"')
 elif num1 == 0 or num2 == 0:
-  print(''"zero"')
+  print('"zero"')
 else:
   print("opposite") 
